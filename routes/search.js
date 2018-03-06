27e92a25a2106bf8bbc6b59411b0ce37b2e0fbeb
Andy Marke <andy.marke@gmail.com>
2018-03-06 11:32:04 +0000

    Use NEW_TECH_BASE_URL env variable

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('search', {  });
+    res.render('search', {baseUrl: process.env.NEW_TECH_BASE_URL});
 });
 
 module.exports = router;
--- routes/sfpsr.js
+++ routes/sfpsr.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('sfpsr', {  });
+    res.render('sfpsr', {baseUrl: process.env.NEW_TECH_BASE_URL});
 });
 
 module.exports = router;
