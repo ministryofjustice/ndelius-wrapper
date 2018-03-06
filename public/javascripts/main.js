b287ad8db1ed4c63c23604c5cc5001c9f90efe9f
Andy Marke <andy.marke@gmail.com>
2018-03-06 10:59:56 +0000

    Initial version

---
+++ public/javascripts/main.js
@@ -0,0 +1,29 @@
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event)
+{
+    if (event.data.indexOf('{') === 0) {
+        var eventData = JSON.parse(event.data)
+
+        switch(eventData.action) {
+            case 'addOffender':
+                window.location = '/add_offender'
+                break
+            case 'toggleSearch':
+                window.location = '/legacy_search'
+                break
+            case 'viewOffender':
+                window.location = '/offender_details'
+                break
+            case 'addContact':
+                window.location = '/add_contact'
+                break
+            default:
+                console.log(eventData)
+        }
+    } else {
+        console.log(event.data)
+        window.location = '/'
+
+    }
+}
