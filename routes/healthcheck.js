4ce23039b7d15386419c991dc3e29c8787dbc92f
Nick Redshaw <nickredshaw@gmail.com>
2018-03-16 12:26:35 +0000

    [NR] 253 - Pretty print the health check

--- routes/healthcheck.js
+++ routes/healthcheck.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 router.get('/', function(req, res, next) {
-    res.render('healthcheck', {baseUrl: process.env.NEW_TECH_BASE_URL});
+    const appUrl = process.env.NEW_TECH_BASE_URL;
+    request(appUrl + 'healthcheck', function (err, response, body) {
+        if (err) throw err;
+        const document = JSON.parse(body);
+        res.render('healthcheck', {healthcheck: JSON.stringify(document, null, 4)  })
+    });
 });
 
 module.exports = router;
