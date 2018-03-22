67cd76a7201f34b408e41ca930f574f28bd41c5f
Andy Marke <andy.marke@gmail.com>
2018-03-22 17:13:02 +0000

    made database configurable

--- routes/view_pdf.js
+++ routes/view_pdf.js
@@ -6,7 +6,7 @@ const {MongoClient, ObjectId} = require('mongodb');
 router.get('/', function(req, res, next) {
     MongoClient.connect(process.env.ANALYTICS_MONGO_CONNECTION || "mongodb://localhost", function(err, db) {
         if (err) throw err;
-        const dbo = db.db("analytics");
+        const dbo = db.db(process.env.ANALYTICS_MONGO_DATABASE || "analytics");
         const query = {_id: ObjectId(req.query.id)}
         dbo.collection("shortFormatReports").findOne(query, (err, result) => {
             if (err) throw err
