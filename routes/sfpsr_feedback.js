f4100134c4b9d6c90b9a87b30d67986320681491
Andy Marke <andy.marke@gmail.com>
2018-04-06 11:05:18 +0100

    141 Short format feedback page (small refactor for search feedback)

--- routes/sfpsr_feedback.js
+++ routes/sfpsr_feedback.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('feedback', {baseUrl: process.env.NEW_TECH_BASE_URL});
+    res.render('sfpsr_feedback', {baseUrl: process.env.NEW_TECH_BASE_URL});
 });
 
 module.exports = router;
