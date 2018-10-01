0d2f161c3f537df423c1f8c1a609af0f20f9485f
Andy Marke <andy.marke@gmail.com>
2018-10-01 17:34:35 +0100

    removed redundant sfpsr parameters

--- routes/sfpsr.js
+++ routes/sfpsr.js
@@ -13,48 +13,13 @@ router.get('/', function(req, res, next) {
             if (err) throw err;
             const document = JSON.parse(body)
             const offender = document._source
-            let pnc = '';
-            if (offender.otherIds.pncNumber) {
-                pnc = encodeURIComponent(encrypt(`${offender.otherIds.pncNumber}`))
-            }
             const crn = encodeURIComponent(encrypt(`${offender.otherIds.crn}`));
-            let address = ''
-            if (offender.contactDetails.addresses && offender.contactDetails.addresses.length > 0) {
-                const offenderAddress = offender.contactDetails.addresses[0]
-                let addressAsLine = ''
-                if (offenderAddress.buildingName) {
-                    addressAsLine = addressAsLine + offenderAddress.buildingName + '\n'
-                }
-                if (offenderAddress.addressNumber) {
-                    addressAsLine = addressAsLine + offenderAddress.addressNumber + '\n'
-                }
-                if (offenderAddress.streetName) {
-                    addressAsLine = addressAsLine + offenderAddress.streetName + '\n'
-                }
-                if (offenderAddress.town) {
-                    addressAsLine = addressAsLine + offenderAddress.town + '\n'
-                }
-                if (offenderAddress.county) {
-                    addressAsLine = addressAsLine + offenderAddress.county + '\n'
-                }
-                if (offenderAddress.postcode) {
-                    addressAsLine = addressAsLine + offenderAddress.postcode + '\n'
-                }
-                address = encodeURIComponent(encrypt(addressAsLine))
-            }
-            const name = encodeURIComponent(encrypt(`${offender.firstName} ${offender.surname}`))
-            const offenderDob = moment(offender.dateOfBirth, 'YYYY-MM-DD')
-            const dateOfBirth = encodeURIComponent(encrypt(offenderDob.format('DD/MM/YYYY')))
-            const age = encodeURIComponent(encrypt(`${Math.floor(moment().diff(offenderDob, 'years', true))}`))
-            res.render('sfpsr', {baseUrl: process.env.NEW_TECH_BASE_URL, name, dateOfBirth, age, pnc, crn, address});
+            res.render('sfpsr', {baseUrl: process.env.NEW_TECH_BASE_URL, crn});
         });
 
     } else {
-        const name = 'xylkFTVA6GXA1GRZZxZ4MA%3D%3D'
-        const dateOfBirth = 'twqjuUftRY5xaB556mJb6A%3D%3D'
         const crn = 'v5LH8B7tJKI7fEc9uM76SQ%3D%3D'
-        const age = 'RRioaTyIHLGnja2CBw8hqg%3D%3D'
-        res.render('sfpsr', {baseUrl: process.env.NEW_TECH_BASE_URL, name, dateOfBirth, age, crn});
+        res.render('sfpsr', {baseUrl: process.env.NEW_TECH_BASE_URL, crn});
     }
 
 });
